3df0a6d36d13a1760560c697cbcf81b08f6f94e7
jimy189 <renatocelo189@gmail.com>
2020-08-19 19:49:53 -0300

    First
    primeiro programa.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               string nome;
+            int quartos;
+            double produto;
+            int idade;
+            double altura;
+
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite quantos quartos tem: ");
+            quartos = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Digite o preço do Produto: ");
+            produto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nome Completo: " + nome);
+            Console.WriteLine("Numero de Quartos: " + quartos);
+            Console.WriteLine("Preço do Produto: " +produto);
+            Console.WriteLine("Nome:" + nome + " Idade: " + idade + " Tamanho: "+ altura);
+        }
+    }
+}
